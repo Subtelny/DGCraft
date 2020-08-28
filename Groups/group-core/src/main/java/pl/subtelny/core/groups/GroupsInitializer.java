368d4460c04c46d0a1accd3f7757bd67aec4eb97
HaKIM <webdevhakim@gmail.com>
2020-08-28 18:26:41 +0200

    Initialized Groups, group-core and group-api modules

---
+++ Groups/group-core/src/main/java/pl/subtelny/core/groups/GroupsInitializer.java
@@ -0,0 +1,13 @@
+package pl.subtelny.core.groups;
+
+import org.bukkit.plugin.Plugin;
+import pl.subtelny.components.core.api.Component;
+import pl.subtelny.components.core.api.DependencyActivator;
+import pl.subtelny.utilities.log.LogUtil;
+
+@Component
+public class GroupsInitializer implements DependencyActivator {
+    @Override
+    public void activate(Plugin plugin) {
+    }
+}
