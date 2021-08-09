4affad0a5ed0f14e1de7a9ec2b012a0042b3d79c
Sebastian J <sebo.janda@gmail.com>
2021-08-09 21:55:25 +0200

    reflection back to 0.9.11

--- Components/components-core/src/main/java/pl/subtelny/components/core/reflections/ComponentReflections.java
+++ Components/components-core/src/main/java/pl/subtelny/components/core/reflections/ComponentReflections.java
@@ -1,9 +1,11 @@
 package pl.subtelny.components.core.reflections;
 
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import org.reflections.ReflectionUtils;
 import org.reflections.Reflections;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -16,10 +18,9 @@ public class ComponentReflections extends Reflections {
     }
 
     public Set<Class<?>> getComponentTypes() {
-        Set<String> classes = getStore().get(ComponentScanner.class, COMPONENT_SCANNER_INDEX);
-        //Multimap<String, String> componentsStore = getStore().get(COMPONENT_SCANNER_INDEX);
-        //Collection<String> values = componentsStore.values();
-        return Sets.newHashSet(ReflectionUtils.forNames(classes, getConfiguration().getClassLoaders()));
+        Multimap<String, String> componentsStore = getStore().get(COMPONENT_SCANNER_INDEX);
+        Collection<String> values = componentsStore.values();
+        return Sets.newHashSet(ReflectionUtils.forNames(values, getConfiguration().getClassLoaders()));
     }
 
 }
--- Components/components-core/src/main/java/pl/subtelny/components/core/reflections/ComponentScanner.java
+++ Components/components-core/src/main/java/pl/subtelny/components/core/reflections/ComponentScanner.java
@@ -1,6 +1,5 @@
 package pl.subtelny.components.core.reflections;
 
-import org.reflections.Store;
 import org.reflections.scanners.TypeAnnotationsScanner;
 import pl.subtelny.components.core.api.Component;
 
@@ -12,19 +11,19 @@ import java.util.Optional;
 public class ComponentScanner extends TypeAnnotationsScanner {
 
     @Override
-    public void scan(Object cls, Store store) {
+    public void scan(Object cls) {
         String className = getMetadataAdapter().getClassName(cls);
         List<String> classAnnotationNames = getMetadataAdapter().getClassAnnotationNames(cls);
         classAnnotationNames.stream()
                 .filter(this::acceptResult)
-                .forEach(annotationName -> computeAnnotation(className, annotationName, store));
+                .forEach(annotationName -> computeAnnotation(className, annotationName));
     }
 
-    private void computeAnnotation(String className, String annotationName, Store store) {
+    private void computeAnnotation(String className, String annotationName) {
         Optional<Class<?>> annotationClassOpt = findClassByName(annotationName);
         if (annotationClassOpt.isPresent()) {
             if (isComponent(annotationClassOpt.get())) {
-                store.put(ComponentScanner.class, annotationName, className);
+                getStore().put(annotationName, className);
             }
         }
     }
